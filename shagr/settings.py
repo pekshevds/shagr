1f3b96cabf019207587f5d429532c862f83325a1
pekshevds <pekshev.ds@gmail.com>
2020-09-23 11:28:44 +0300

    create base app

--- shagr/settings.py
+++ shagr/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'basesapp',
     'goodsapp',
 ]
 
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'shagr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'shagr/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

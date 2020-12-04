3786798b7fc13a303482f90bb8fe5782ef4b7c6e
denis <pekshev.ds@gmail.com>
2020-12-04 15:58:10 +0300

    20201204_002 pagination and errors

--- catalogapp/views.py
+++ catalogapp/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
-from .main import get_hierarchy_categoryes, find_category_by_slug, find_good_by_slug, get_childs, get_goods
-from .models import Category, Good
+
+from .main import get_hierarchy_categoryes
+from .main import find_category_by_slug
+from .main import find_good_by_slug
+from .main import get_childs
+from .main import get_goods
+from .main import get_properties_and_values
+
+from django.core.paginator import Paginator
+
+def render_list(request, parent):
+
+	goods_count = 15
+
+	childs = get_childs(parent=parent)
+	goods = get_goods(category=parent)
+	
+
+	page_number = request.GET.get('page', 1)
+	paginator = Paginator(goods, goods_count)
+	page = paginator.get_page(page_number)
+	is_paginated = page.has_other_pages()
+
+
+
+	context = {
+
+		'categories'	: get_hierarchy_categoryes(),
+		'parent'		: parent,
+		'childs'		: childs,
+		'goods_count'	: len(goods),
+		'page'			: page,
+		'is_paginated'	: is_paginated,
+
+	}	
+	return render(request, 'catalogapp/list.html', context)
+
 
 def show_catalog(request):
 
@@ -8,7 +43,6 @@ def show_catalog(request):
 
 
 def show_list(request, slug):
-
 	
 	return render_list(request, find_category_by_slug(slug))
 
@@ -17,31 +51,15 @@ def show_item(request, slug):
 
 	good = find_good_by_slug(slug=slug)
 	childs = get_childs(parent=good.category)
+	properties_and_values = get_properties_and_values(good=good)
 
 	context = {
 
-		'categories': get_hierarchy_categoryes(),
-		'parent'	: good.category,
-		'childs'	: childs,
-		'good'	    : good,
+		'categories'			: get_hierarchy_categoryes(),
+		'parent'				: good.category,
+		'childs'				: childs,
+		'good'	    			: good,
+		'properties_and_values'	: properties_and_values,
 
 	}
 	return render(request, 'catalogapp/item.html', context)
-
-
-def render_list(request, parent):
-
-	childs = get_childs(parent=parent)
-	goods = get_goods(category=parent)
-	
-
-	context = {
-
-		'categories': get_hierarchy_categoryes(),
-		'parent'	: parent,
-		'childs'	: childs,
-		'goods'		: goods,
-
-	}
-	
-	return render(request, 'catalogapp/list.html', context)

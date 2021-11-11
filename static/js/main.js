82b794530080b8c56dbb8930104518f527e40e73
MaximPekshev <mp198769@gmail.com>
2021-11-11 16:01:27 +0300

    commit after collectstatic

--- static/js/main.js
+++ static/js/main.js
@@ -693,7 +693,44 @@
             }
         });
     });
+    // as
+    $(function () {
+        $('[name="checkout_delivery_type"]').on('change', function () {
+            const currentItem = $(this).closest('.payment-methods__item');
 
+            $(this).closest('.payment-methods__list').find('.payment-methods__item').each(function (i, element) {
+                const links = $(element);
+                const linksContent = links.find('.payment-methods__item-container');
+
+                if (element !== currentItem[0]) {
+                    const startHeight = linksContent.height();
+
+                    linksContent.css('height', startHeight + 'px');
+                    links.removeClass('payment-methods__item--active');
+
+                    links.height(); // force reflow
+                    linksContent.css('height', '');
+                } else {
+                    const startHeight = linksContent.height();
+
+                    links.addClass('payment-methods__item--active');
+
+                    const endHeight = linksContent.height();
+
+                    linksContent.css('height', startHeight + 'px');
+                    links.height(); // force reflow
+                    linksContent.css('height', endHeight + 'px');
+                }
+            });
+        });
+
+        $('.payment-methods__item-container').on('transitionend', function (event) {
+            if (event.originalEvent.propertyName === 'height') {
+                $(this).css('height', '');
+            }
+        });
+    });
+    // as
 
     /*
     // add-vehicle-modal
